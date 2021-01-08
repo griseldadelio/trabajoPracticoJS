34c3c1ce99056a53b3018f9efc2c935a4ab75a4a
griselda de lio <griseldadelio@hotmail.com>
2021-01-08 15:47:48 -0300

    arme modal de funciones edit y delete

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
-const urlBase = 'https://5ff3193428c3980017b18f70.mockapi.io';
 const tableBody = document.getElementById('users-information')
 const editIcon = document.getElementsByClassName("edit");
 const deleteIcon = document.getElementsByClassName("delete");
+const form = document.getElementById("formUserInformation");
+const urlBase = 'https://5ff3193428c3980017b18f70.mockapi.io';
+
+const createObject = () => {
+    const fullname = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const address = document.getElementById('address').value;
+    const phone = document.getElementById('phone').value;
+
+    return { fullname, email, address, phone }
+}
+
+const validateObject = (user) => {
+    validateEmail(user.email);
+    validatePhone(user.phone);
+
+    if (user.fullname.length > 50) {
+        alert('Exceso de caracteres')
+    }
+
+    if (user.address > 60) {
+        alert('Exceso de caracteres')
+    }
+}
 
 /* Funcion para traer los datos */
 
@@ -16,6 +39,7 @@ const getUsers = () => {
 }
 getUsers()
 
+
 /* Funcion para mostrar los usuarios en tabla */
 
 const showUsers = (data) => {
@@ -29,13 +53,42 @@ const showUsers = (data) => {
         <td>${element.address}</td>
         <td>${element.phone}</td>
         <td>
-            <i class="material-icons edit" id="${element.id}"  title="Edit">&#xE254;</i>
-            <i class="material-icons delete" id="${element.id}" title="Delete">&#xE872;</i>
+            <i class="material-icons edit bg-warning" id="${element.id}"  title="Edit">&#xE254;</i>
+            <i class="material-icons delete bg-danger" id="${element.id}" title="Delete">&#xE872;</i>
         </td> 
         </tr>`
     });
     tableBody.innerHTML = dataBase;
 }
+
+
+/* Funcion para crear Usuario nuevo */
+
+const registerUser = (e) => {
+    e.preventDefault()
+    const user = createObject();
+    if (validateObject(user) == false) {
+        return
+    }
+    fetch(`${urlBase}/users`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'Application/json'
+        },
+        body: JSON.stringify(user)
+    })
+        .then(() => {
+            alert('Registro generado exitosamente')
+            location.reload()
+        })
+        .catch(error => {
+            console.error(error)
+            alert('Se ha producido un error')
+        })
+}
+
+form.addEventListener('submit', registerUser);
+
 
 /* Funcion para editar los datos del Usuario */
 
@@ -51,10 +104,7 @@ const editUsers = (data) => {
                     const save = document.getElementById('edit')
 
                     save.onclick = () => {
-                        const name = document.getElementById("employee_name").value
-                        const email = document.getElementById("employee_email").value
-                        const address = document.getElementById("employee_address").value
-                        const phone = document.getElementById("employee_phone").value
+                        createObject()
                         const newEmployee = {
                             fullname: name,
                             email: email,
@@ -77,6 +127,7 @@ const editUsers = (data) => {
     }
 }
 
+
 /* Funcion para eliminar Usuario */
 
 const deleteUsers = (data) => {
@@ -85,7 +136,6 @@ const deleteUsers = (data) => {
             const userRemove = deleteIcon[i].id
             data.forEach(element => {
                 if (element.id == userRemove) {
-                    alert('Para guardar sus cambios presione save, al aceptar esta ventana')
                     modal.classList.remove('nomostrar')
                     modalNewEmployee(element.fullname, element.email, element.address, element.phone)
 
@@ -107,9 +157,9 @@ const deleteUsers = (data) => {
                 }
             });
         }
-
     }
 }
+
 
 /* Funcion para Filtrar lista de usuarios */
 
@@ -131,24 +181,49 @@ const filterUsers = () => {
 filterUsers()
 
 
-//LO ARME PARA NO PISAR LO TUYO, LO DEBERIAMOS ADAPTAR
+/* Modal para las funciones edit y delete */
 
 const modalNewEmployee = (name = "", email = "", address = "", phone = "") => {
     modal.innerHTML = `
-    <form action="" method="get" class="modal-form">
-      <label>Name</label>
-        <input type="text" id="employee_name" maxlength="50" value=${name}>
-      <label>Email</label>
-        <input type="text" id="employee_email" value=${email}>
-      <label>Address</label>
-        <input type="text" name="Address" id="employee_address" maxlength="60" value=${address}> 
-      <label>Phone</label><br>
-        <input type="text" id="employee_phone" value=${phone}>
-    </form>
-    <div class="div-button">
-        <button id="cancel">Cancel</button>
-        <button id="edit">Save</button>
-    </div>`
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" > Employee Info</h5>
+                </div>        
+                <div class="modal-body" id="modalBody">
+                    <div class="container-fluid">
+                        <div class="form-group column">
+                            <label for="name" class="col-sm-2 col-form-label">Name</label>
+                                <div class="col-sm-10">
+                                    <input type="text" id="name" class="form-control"  maxlength="50" value=${name}>
+                                </div>
+                        </div>
+                        <div class="form-group column">
+                            <label for="name" class="col-sm-2 col-form-label">Email</label>
+                                <div class="col-sm-10">
+                                    <input type="email" id="email" class="form-control"  maxlength="50" value=${email}>
+                                </div>
+                        </div>
+                        <div class="form-group column">
+                            <label for="name" class="col-sm-2 col-form-label">Adress</label>
+                                <div class="col-sm-10">
+                                    <textarea id="address" class="form-control" value=${address}></textarea>
+                                </div>
+                        </div>
+                        <div class="form-group column">
+                            <label for="name" class="col-sm-2 col-form-label">Phone</label>
+                            <div class="col-sm-10">
+                                <input type="text" id="phone" class="form-control"  maxlength="50" value=${phone}>
+                            </div>
+                        </div>                         
+                    </div>                 
+                </div>  
+                <div class="modal-footer bg-light">
+                    <button id='cancel' class="btn btn-light" data-bs-dismiss="modal">Cancel</button>
+                    <button id='edit' class="btn btn-success">Save</button>
+                </div>                                      
+            </div>
+        </div>`
     const cancel = document.getElementById("cancel");
     cancel.onclick = () => {
         modal.classList.add("nomostrar");
